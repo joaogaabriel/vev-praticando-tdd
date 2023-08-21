dba73d232422c168e2441bd77eccc57eb216ec9c
caiovalverde20 <caiovalverde20@gmail.com>
2023-08-21 01:34:14 -0300

    Rep Base e Tentativa de Primeiro teste

---
+++ NotaFiscal/src/test/java/vev/GeradorNotaFiscalTest.java
@@ -0,0 +1,18 @@
+package vev;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class GeradorNotaFiscalTest {
+
+    @Test
+    public void testGerarNotaFiscalParaConsultoria() {
+        Fatura fatura = new Fatura("Cliente A", "Rua X", "CONSULTORIA", 1000.00);
+        GeradorNotaFiscal gerador = new GeradorNotaFiscal();
+
+        NotaFiscal nf = gerador.gera(fatura);
+
+        assertEquals(1000.00, nf.getValor(), 0.01);
+        assertEquals(250.00, nf.getImposto(), 0.01);
+    }
+}
