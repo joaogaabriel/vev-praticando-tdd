c0233be609ab065030263d83d6046d9a430bb2e0
caiovalverde20 <caiovalverde20@gmail.com>
2023-09-25 21:26:06 -0300

    Fix: Diretorio junit 5

--- junit5Tests/Boletos.java
+++ junit5Tests/Boletos.java
@@ -1,10 +1,12 @@
+package junit5Tests;
+
 import Main.Boleto.*;
 import Main.Boleto.Enums.*;
 import org.junit.jupiter.api.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Boletos{
+public class Boletos {
 
     @Nested
     @DisplayName("Testes para Valores de Boletos e Faturas")
@@ -13,89 +15,90 @@ public class Boletos{
         @Test
         @DisplayName("Valor Limite Inferior: 1499")
         void testValorLimiteInferior() {
-            testScenario(1500, new double[]{500, 400, 599}, status.AGUARDANDO_PAGAMENTO, 3);
+            testScenario(1500, new double[] { 500, 400, 599 }, status.AGUARDANDO_PAGAMENTO, 3);
         }
 
         @Test
         @DisplayName("No Limite: 1500")
         void testNoLimite() {
-            testScenario(1500, new double[]{500, 400, 600}, status.PAGO, 3);
+            testScenario(1500, new double[] { 500, 400, 600 }, status.PAGO, 3);
         }
 
         @Test
         @DisplayName("Valor Limite Superior: 1501")
         void testValorLimiteSuperior() {
-            testScenario(1500, new double[]{500, 400, 601}, status.PAGO, 3);
+            testScenario(1500, new double[] { 500, 400, 601 }, status.PAGO, 3);
         }
 
         @Test
         @DisplayName("Todos os boletos somam para o valor exato da fatura")
         void testParticaoValorExato() {
-            testScenario(1500, new double[]{500, 400, 600}, status.PAGO, 3);
+            testScenario(1500, new double[] { 500, 400, 600 }, status.PAGO, 3);
         }
 
         @Test
         @DisplayName("Nenhum pagamento feito")
         void testParticaoNenhumPagamento() {
-            testScenario(1500, new double[]{0, 0, 0}, status.AGUARDANDO_PAGAMENTO, 3);
+            testScenario(1500, new double[] { 0, 0, 0 }, status.AGUARDANDO_PAGAMENTO, 3);
         }
 
         @Test
         @DisplayName("Um único boleto paga toda a fatura")
         void testParticaoUmBoleto() {
-            testScenario(1500, new double[]{1500, 0, 0}, status.PAGO, 1);
+            testScenario(1500, new double[] { 1500, 0, 0 }, status.PAGO, 1);
         }
 
         @Test
         @DisplayName("Dois boletos pagam toda a fatura")
         void testParticaoDoisBoletos() {
-            testScenario(1500, new double[]{750, 750, 0}, status.PAGO, 2);
-        }
-    
-    @Nested
-    @DisplayName("Testes para Tabelas de Decisão")
-    class TabelasDecisaoTest {
-
-        @Test
-        @DisplayName("Cenário comum de três boletos que pagam a fatura")
-        void testDecisaoCenarioComum() {
-            testScenario(1500, new double[]{500, 400, 600}, status.PAGO, 3);
+            testScenario(1500, new double[] { 750, 750, 0 }, status.PAGO, 2);
         }
 
-        @Test
-        @DisplayName("Um único boleto paga toda a fatura")
-        void testDecisaoUmBoleto() {
-            testScenario(1500, new double[]{0, 0, 1500}, status.PAGO, 1);
+        @Nested
+        @DisplayName("Testes para Tabelas de Decisão")
+        class TabelasDecisaoTest {
+
+            @Test
+            @DisplayName("Cenário comum de três boletos que pagam a fatura")
+            void testDecisaoCenarioComum() {
+                testScenario(1500, new double[] { 500, 400, 600 }, status.PAGO, 3);
+            }
+
+            @Test
+            @DisplayName("Um único boleto paga toda a fatura")
+            void testDecisaoUmBoleto() {
+                testScenario(1500, new double[] { 0, 0, 1500 }, status.PAGO, 1);
+            }
+
+            @Test
+            @DisplayName("Nenhuma fatura, nenhum pagamento")
+            void testDecisaoNenhumaFatura() {
+                testScenario(0, new double[] { 0, 0, 0 }, status.AGUARDANDO_PAGAMENTO, 0);
+            }
+
+            @Test
+            @DisplayName("Três boletos de 500 pagam a fatura de 1500")
+            void testDecisaoTresBoletos() {
+                testScenario(1500, new double[] { 500, 500, 500 }, status.PAGO, 3);
+            }
         }
 
-        @Test
-        @DisplayName("Nenhuma fatura, nenhum pagamento")
-        void testDecisaoNenhumaFatura() {
-            testScenario(0, new double[]{0, 0, 0}, status.AGUARDANDO_PAGAMENTO, 0);
-        }
+        private void testScenario(double valorFatura, double[] valoresBoletos, status expectedStatus,
+                int expectedPayments) {
+            List<Boleto> boletosList = new ArrayList<>();
+            for (double valor : valoresBoletos) {
+                boletosList.add(new Boleto("01/01/2022", valor, "Cliente"));
+            }
+            Processador processador = new Processador();
+            Fatura fatura = new Fatura("01/01/2022", valorFatura, "Cliente");
 
-        @Test
-        @DisplayName("Três boletos de 500 pagam a fatura de 1500")
-        void testDecisaoTresBoletos() {
-            testScenario(1500, new double[]{500, 500, 500}, status.PAGO, 3);
+            List<Pagamento> pagamentos = processador.processaBoletos(boletosList, fatura);
+
+            Assertions.assertEquals(expectedPayments, pagamentos.size());
+            Assertions.assertEquals(expectedStatus, fatura.getStatus());
         }
     }
 
-    private void testScenario(double valorFatura, double[] valoresBoletos, status expectedStatus,
-                              int expectedPayments) {
-        List<Boleto> boletosList = new ArrayList<>();
-        for (double valor : valoresBoletos) {
-            boletosList.add(new Boleto("01/01/2022", valor, "Cliente"));
-        }
-        Processador processador = new Processador();
-        Fatura fatura = new Fatura("01/01/2022", valorFatura, "Cliente");
-
-        List<Pagamento> pagamentos = processador.processaBoletos(boletosList, fatura);
-
-        Assertions.assertEquals(expectedPayments, pagamentos.size());
-        Assertions.assertEquals(expectedStatus, fatura.getStatus());
-    }
-}
     // Testes para cenários de pagamentos parciais
     @Test
     @DisplayName("Dois pagamentos parciais somam ao valor da fatura")
@@ -128,7 +131,8 @@ public class Boletos{
         testScenarioWithDueDate(1500, new double[] { 500, 400, 600 }, "03/01/2022", status.AGUARDANDO_PAGAMENTO, 3);
     }
 
-    private void testScenarioWithDueDate(double valorFatura, double[] valoresBoletos, String dataVencimento, status expectedStatus, int expectedPayments) {
+    private void testScenarioWithDueDate(double valorFatura, double[] valoresBoletos, String dataVencimento,
+            status expectedStatus, int expectedPayments) {
         List<Boleto> boletosList = new ArrayList<>();
         for (double valor : valoresBoletos) {
             boletosList.add(new Boleto(dataVencimento, valor, "Cliente"));
---
+++ junit5Tests/GeradorNotaFiscal .java
@@ -0,0 +1,137 @@
+package functionalTests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import Main.NotaFiscal.Fatura;
+import Main.NotaFiscal.GeradorNotaFiscal;
+import Main.NotaFiscal.NotaFiscal;
+
+@DisplayName("Testes para GeradorNotaFiscal")
+public class GeradorNotaFiscalTest {
+
+    GeradorNotaFiscal gerador = new GeradorNotaFiscal();
+
+    @Nested
+    @DisplayName("Testes de Limite")
+    class LimiteTests {
+
+        @Nested
+        @DisplayName("Consultoria")
+        class ConsultoriaTests {
+            @Test
+            @DisplayName("Limite Menor")
+            public void testLimiteConsultoriaMenor() {
+                Fatura fatura = new Fatura("Cliente A", "Rua X", "CONSULTORIA", 999.99);
+                NotaFiscal nf = gerador.gera(fatura);
+                assertEquals(999.99, nf.getValor(), 0.01);
+                assertEquals(249.9975, nf.getImposto(), 0.01);
+            }
+
+            @Test
+            @DisplayName("Limite Maior")
+            public void testLimiteConsultoriaMaior() {
+                Fatura fatura = new Fatura("Cliente B", "Rua Y", "CONSULTORIA", 1000.00);
+                NotaFiscal nf = gerador.gera(fatura);
+                assertEquals(1000.00, nf.getValor(), 0.01);
+                assertEquals(250.00, nf.getImposto(), 0.01);
+            }
+        }
+
+        @Nested
+        @DisplayName("Treinamento")
+        class TreinamentoTests {
+            @Test
+            @DisplayName("Limite Menor")
+            public void testLimiteTreinamentoMenor() {
+                Fatura fatura = new Fatura("Cliente C", "Rua Z", "TREINAMENTO", 999.99);
+                NotaFiscal nf = gerador.gera(fatura);
+                assertEquals(999.99, nf.getValor(), 0.01);
+                assertEquals(149.9985, nf.getImposto(), 0.01);
+            }
+
+            @Test
+            @DisplayName("Limite Maior")
+            public void testLimiteTreinamentoMaior() {
+                Fatura fatura = new Fatura("Cliente D", "Rua W", "TREINAMENTO", 1000.00);
+                NotaFiscal nf = gerador.gera(fatura);
+                assertEquals(1000.00, nf.getValor(), 0.01);
+                assertEquals(150.00, nf.getImposto(), 0.01);
+            }
+        }
+
+        @Nested
+        @DisplayName("Outros")
+        class OutrosTests {
+            @Test
+            @DisplayName("Limite Menor")
+            public void testLimiteOutrosMenor() {
+                Fatura fatura = new Fatura("Cliente E", "Rua V", "OUTROS", 999.99);
+                NotaFiscal nf = gerador.gera(fatura);
+                assertEquals(999.99, nf.getValor(), 0.01);
+                assertEquals(59.9994, nf.getImposto(), 0.01);
+            }
+
+            @Test
+            @DisplayName("Limite Maior")
+            public void testLimiteOutrosMaior() {
+                Fatura fatura = new Fatura("Cliente F", "Rua U", "OUTROS", 1000.00);
+                NotaFiscal nf = gerador.gera(fatura);
+                assertEquals(1000.00, nf.getValor(), 0.01);
+                assertEquals(60.00, nf.getImposto(), 0.01);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("Testes de Sucesso")
+    class SucessoTests {
+
+        @Test
+        @DisplayName("Consultoria")
+        public void testConsultoriaComSucesso() {
+            Fatura fatura = new Fatura("João", "Endereço qualquer", "CONSULTORIA", 100);
+            NotaFiscal nf = gerador.gera(fatura);
+            assertEquals(100, nf.getValor(), 0.01);
+            assertEquals(25, nf.getImposto(), 0.01);
+        }
+
+        @Test
+        @DisplayName("Treinamento")
+        public void testTreinamentoComSucesso() {
+            Fatura fatura = new Fatura("Ana", "Endereço qualquer", "TREINAMENTO", 100);
+            NotaFiscal nf = gerador.gera(fatura);
+            assertEquals(100, nf.getValor(), 0.01);
+            assertEquals(15, nf.getImposto(), 0.01);
+        }
+
+        @Test
+        @DisplayName("Outros")
+        public void testOutroComSucesso() {
+            Fatura fatura = new Fatura("Maria", "Endereço qualquer", "OUTROS", 100);
+            NotaFiscal nf = gerador.gera(fatura);
+            assertEquals(100, nf.getValor(), 0.01);
+            assertEquals(6, nf.getImposto(), 0.01);
+        }
+    }
+
+    @Nested
+    @DisplayName("Testes de Falha")
+    class FalhaTests {
+
+        @Test
+        @DisplayName("Tipo Inválido")
+        public void testFalhaTipoInvalido() {
+            Fatura fatura = new Fatura("João", "Endereço qualquer", "INVALIDO", 100);
+            assertThrows(IllegalArgumentException.class, () -> gerador.gera(fatura));
+        }
+
+        @Test
+        @DisplayName("Sem Cliente")
+        public void testFalhaSemCliente() {
+            Fatura fatura = new Fatura("", "Endereço qualquer", "CONSULTORIA", 100);
+            assertThrows(IllegalArgumentException.class, () -> gerador.gera(fatura));
+        }
+    }
+}
